16c64a52c5d0a1cd06c55ad928ddefc1e8f9dc31
Jitta Nagasai Reddy <jittanagasai16@gmail.com>
2026-06-23 11:40:58 +0530

    updated

--- Stock.java
+++ Stock.java
@@ -4,7 +4,7 @@ class Stock{
         int n = prices.length;
         int res = 0;
 
-        // Explore all possible ways to buy and sell stock
+        
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
                 res = Math.max(res, prices[j] - prices[i]);
